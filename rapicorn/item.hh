78ef8b67fe5cabab27ed878d640887da9092c22d
Tim Janik <timj@gtk.org>
2006-06-01 00:00:33 +0000

    formatting fixes

--- rapicorn/item.hh
+++ rapicorn/item.hh
@@ -60,53 +60,53 @@ public:
 
 /* --- Item --- */
 class Item : public virtual Convertible, public virtual DataListContainer, public virtual ReferenceCountImpl {
-  /*Copy*/                      Item      (const Item&);
-  Item&                         operator= (const Item&);
-  Item                         *m_parent;       /* interface-inlined for fast read-out */
-  uint32                        m_flags;        /* interface-inlined for fast read-out */
-  Style                        *m_style;
+  /*Copy*/                    Item      (const Item&);
+  Item&                       operator= (const Item&);
+  Item                       *m_parent;         /* interface-inlined for fast read-out */
+  uint32                      m_flags;          /* interface-inlined for fast read-out */
+  Style                      *m_style;
   friend class Container;
-  void                          propagate_flags ();
-  void                          propagate_style ();
+  void                        propagate_flags   ();
+  void                        propagate_style   ();
 protected:
   /* flag handling */
-  bool                          change_flags_silently (uint32 mask, bool on);
+  bool                        change_flags_silently   (uint32 mask, bool on);
   enum {
-    ANCHORED                    = 1 <<  0,
-    VISIBLE                     = 1 <<  1,
-    HIDDEN_CHILD                = 1 <<  2,
-    SENSITIVE                   = 1 <<  3,
-    PARENT_SENSITIVE            = 1 <<  4,
-    PRELIGHT                    = 1 <<  5,
-    IMPRESSED                   = 1 <<  6,
-    HAS_FOCUS                   = 1 <<  7,
-    HAS_DEFAULT                 = 1 <<  8,
-    /* REQUEST_DEFAULT          = 1 <<  8, */
-    INVALID_REQUISITION         = 1 << 10,
-    INVALID_ALLOCATION          = 1 << 11,
-    EXPOSE_ON_CHANGE            = 1 << 12,
-    INVALIDATE_ON_CHANGE        = 1 << 13,
-    HEXPAND                     = 1 << 14,
-    VEXPAND                     = 1 << 15,
-    HSPREAD                     = 1 << 16,
-    VSPREAD                     = 1 << 17,
-    HSPREAD_CONTAINER           = 1 << 18,
-    VSPREAD_CONTAINER           = 1 << 19,
-    POSITIVE_ALLOCATION         = 1 << 20,
-    DEBUG                       = 1 << 21,
-    LAST_FLAG                   = 1 << 22
+    ANCHORED                  = 1 <<  0,
+    VISIBLE                   = 1 <<  1,
+    HIDDEN_CHILD              = 1 <<  2,
+    SENSITIVE                 = 1 <<  3,
+    PARENT_SENSITIVE          = 1 <<  4,
+    PRELIGHT                  = 1 <<  5,
+    IMPRESSED                 = 1 <<  6,
+    HAS_FOCUS                 = 1 <<  7,
+    HAS_DEFAULT               = 1 <<  8,
+    /* REQUEST_DEFAULT        = 1 <<  8, */
+    INVALID_REQUISITION       = 1 << 10,
+    INVALID_ALLOCATION        = 1 << 11,
+    EXPOSE_ON_CHANGE          = 1 << 12,
+    INVALIDATE_ON_CHANGE      = 1 << 13,
+    HEXPAND                   = 1 << 14,
+    VEXPAND                   = 1 << 15,
+    HSPREAD                   = 1 << 16,
+    VSPREAD                   = 1 << 17,
+    HSPREAD_CONTAINER         = 1 << 18,
+    VSPREAD_CONTAINER         = 1 << 19,
+    POSITIVE_ALLOCATION       = 1 << 20,
+    DEBUG                     = 1 << 21,
+    LAST_FLAG                 = 1 << 22
   };
-  void                          set_flag        (uint32 flag, bool on = true);
-  void                          unset_flag      (uint32 flag) { set_flag (flag, false); }
-  bool                          test_flags      (uint32 mask) const { return (m_flags & mask) == mask; }
-  bool                          test_any_flag   (uint32 mask) const { return (m_flags & mask) != 0; }
+  void                        set_flag          (uint32 flag, bool on = true);
+  void                        unset_flag        (uint32 flag) { set_flag (flag, false); }
+  bool                        test_flags        (uint32 mask) const { return (m_flags & mask) == mask; }
+  bool                        test_any_flag     (uint32 mask) const { return (m_flags & mask) != 0; }
   /* size requisition and allocation */
-  virtual void                  size_request    (Requisition &requisition) = 0;
-  virtual void                  size_allocate   (Allocation area) = 0;
+  virtual void                size_request      (Requisition &requisition) = 0;
+  virtual void                size_allocate     (Allocation area) = 0;
   /* signal methods */
-  virtual bool                  match_interface (InterfaceMatch &imatch, const String &ident);
-  virtual void                  do_invalidate   () = 0;
-  virtual void                  do_changed      () = 0;
+  virtual bool                match_interface   (InterfaceMatch &imatch, const String &ident);
+  virtual void                do_invalidate     () = 0;
+  virtual void                do_changed        () = 0;
   /* misc */
   virtual                     ~Item             ();
   virtual void                style             (Style  *st);
@@ -114,60 +114,60 @@ protected:
   virtual void                hierarchy_changed (Item *old_toplevel);
   void                        anchored          (bool b) { set_flag (ANCHORED, b); }
 public:
-  explicit                      Item            ();
-  bool                          anchored        () const { return test_flags (ANCHORED); }
-  bool                          visible         () const { return test_flags (VISIBLE) && !test_flags (HIDDEN_CHILD); }
-  void                          visible         (bool b) { set_flag (VISIBLE, b); }
-  bool                          sensitive       () const { return test_flags (SENSITIVE | PARENT_SENSITIVE); }
-  virtual void                  sensitive       (bool b);
-  bool                          insensitive     () const { return !sensitive(); }
-  void                          insensitive     (bool b) { sensitive (!b); }
-  bool                          prelight        () const { return test_flags (PRELIGHT); }
-  virtual void                  prelight        (bool b);
-  bool                          branch_prelight () const;
-  bool                          impressed       () const { return test_flags (IMPRESSED); }
-  virtual void                  impressed       (bool b);
-  bool                          branch_impressed() const;
-  bool                          has_focus       () const { return test_flags (HAS_FOCUS); }
-  bool                          grab_focus      () const;
-  bool                          has_default     () const { return test_flags (HAS_DEFAULT); }
-  bool                          grab_default    () const;
-  bool                          hexpand         () const { return test_any_flag (HEXPAND | HSPREAD | HSPREAD_CONTAINER); }
-  void                          hexpand         (bool b) { set_flag (HEXPAND, b); }
-  bool                          vexpand         () const { return test_any_flag (VEXPAND | VSPREAD | VSPREAD_CONTAINER); }
-  void                          vexpand         (bool b) { set_flag (VEXPAND, b); }
-  bool                          hspread         () const { return test_any_flag (HSPREAD | HSPREAD_CONTAINER); }
-  void                          hspread         (bool b) { set_flag (HSPREAD, b); }
-  bool                          vspread         () const { return test_any_flag (VSPREAD | VSPREAD_CONTAINER); }
-  void                          vspread         (bool b) { set_flag (VSPREAD, b); }
-  bool                          drawable        () const { return visible() && test_flags (POSITIVE_ALLOCATION); }
-  bool                          debug           () const { return test_flags (DEBUG); }
-  void                          debug           (bool f) { set_flag (DEBUG, f); }
-  virtual String                name            () const = 0;
-  virtual void                  name            (const String &str) = 0;
+  explicit                    Item              ();
+  bool                        anchored          () const { return test_flags (ANCHORED); }
+  bool                        visible           () const { return test_flags (VISIBLE) && !test_flags (HIDDEN_CHILD); }
+  void                        visible           (bool b) { set_flag (VISIBLE, b); }
+  bool                        sensitive         () const { return test_flags (SENSITIVE | PARENT_SENSITIVE); }
+  virtual void                sensitive         (bool b);
+  bool                        insensitive       () const { return !sensitive(); }
+  void                        insensitive       (bool b) { sensitive (!b); }
+  bool                        prelight          () const { return test_flags (PRELIGHT); }
+  virtual void                prelight          (bool b);
+  bool                        branch_prelight   () const;
+  bool                        impressed         () const { return test_flags (IMPRESSED); }
+  virtual void                impressed         (bool b);
+  bool                        branch_impressed  () const;
+  bool                        has_focus         () const { return test_flags (HAS_FOCUS); }
+  bool                        grab_focus        () const;
+  bool                        has_default       () const { return test_flags (HAS_DEFAULT); }
+  bool                        grab_default      () const;
+  bool                        hexpand           () const { return test_any_flag (HEXPAND | HSPREAD | HSPREAD_CONTAINER); }
+  void                        hexpand           (bool b) { set_flag (HEXPAND, b); }
+  bool                        vexpand           () const { return test_any_flag (VEXPAND | VSPREAD | VSPREAD_CONTAINER); }
+  void                        vexpand           (bool b) { set_flag (VEXPAND, b); }
+  bool                        hspread           () const { return test_any_flag (HSPREAD | HSPREAD_CONTAINER); }
+  void                        hspread           (bool b) { set_flag (HSPREAD, b); }
+  bool                        vspread           () const { return test_any_flag (VSPREAD | VSPREAD_CONTAINER); }
+  void                        vspread           (bool b) { set_flag (VSPREAD, b); }
+  bool                        drawable          () const { return visible() && test_flags (POSITIVE_ALLOCATION); }
+  bool                        debug             () const { return test_flags (DEBUG); }
+  void                        debug             (bool f) { set_flag (DEBUG, f); }
+  virtual String              name              () const = 0;
+  virtual void                name              (const String &str) = 0;
   /* properties */
-  void                          set_property    (const String    &property_name,
+  void                        set_property      (const String    &property_name,
                                                  const String    &value,
                                                  const nothrow_t &nt = dothrow);
-  String                        get_property    (const String    &property_name);
-  Property*                     lookup_property (const String    &property_name);
-  virtual const PropertyList&   list_properties ();
+  String                      get_property      (const String    &property_name);
+  Property*                   lookup_property   (const String    &property_name);
+  virtual const PropertyList& list_properties   ();
   /* commands */
-  bool                          exec_command    (const String    &command_call_string,
+  bool                        exec_command      (const String    &command_call_string,
                                                  const nothrow_t &nt = dothrow);
-  Command*                      lookup_command  (const String    &command_name);
-  virtual const CommandList&    list_commands   ();
+  Command*                    lookup_command    (const String    &command_name);
+  virtual const CommandList&  list_commands     ();
   /* parents */
-  virtual void                  set_parent      (Item *parent);
-  Item*                         parent          () const { return m_parent; }
-  Container*                    parent_container() const;
-  bool                          has_ancestor    (const Item &ancestor) const;
-  Root*                         root            ();
+  virtual void                set_parent        (Item *parent);
+  Item*                       parent            () const { return m_parent; }
+  Container*                  parent_container  () const;
+  bool                        has_ancestor      (const Item &ancestor) const;
+  Root*                       root              ();
   /* invalidation / changes */
-  void                          invalidate      ();
-  void                          changed         ();
-  virtual void                  expose          (const Allocation &area) = 0;
-  void                          expose          ()       { expose (allocation()); }
+  void                        invalidate        ();
+  void                        changed           ();
+  virtual void                expose            (const Allocation &area) = 0;
+  void                        expose            ()       { expose (allocation()); }
   /* public signals */
   SignalFinalize<Item>            sig_finalize;
   Signal<Item, void ()>           sig_changed;
@@ -175,17 +175,17 @@ public:
   Signal<Item, void (Item *oldt)> sig_hierarchy_changed;
 public:
   /* event handling */
-  bool                          process_event   (Event     &event);
-  virtual bool                  point           (double     x,  /* global coordinate system */
+  bool                        process_event     (Event     &event);
+  virtual bool                point             (double     x,  /* global coordinate system */
                                                  double     y,
                                                  Affine     affine) = 0;
   /* public size accessors */
-  virtual const Requisition&    size_request    () = 0;                       /* re-request size */
-  const Requisition&            requisition     () { return size_request(); } /* cached requisition */
-  virtual void                  set_allocation  (const Allocation &area) = 0; /* assign new allocation */
-  virtual const Allocation&     allocation      () = 0;                       /* current allocation */
+  virtual const Requisition&  size_request      () = 0;                       /* re-request size */
+  const Requisition&          requisition       () { return size_request(); } /* cached requisition */
+  virtual void                set_allocation    (const Allocation &area) = 0; /* assign new allocation */
+  virtual const Allocation&   allocation        () = 0;                       /* current allocation */
   /* display */
-  virtual void                  render          (Display        &display) = 0;
+  virtual void                render            (Display        &display) = 0;
   /* styles / appearance */
   StateType             state                   () const;
   Style*                style                   () { return m_style; }
