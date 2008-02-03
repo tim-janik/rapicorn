1d0d9700961d08ad8e326c3830dd96c3087c919e
Tim Janik <timj@gtk.org>
2008-02-03 03:27:06 +0100

    Started Rapicorn language binding for Python.
    * Makefile.am: added pyrapicorn/.
    
    * configure.in: build pyrapicorn/Makefile.
    
    * pyrapicorn/Makefile.am: build Python Rapicorn module,
    install Python module files, perform simple Module test.
    
    * pyrapicorn/__init__.py: initialize Python Rapicorn module.
    
    * pyrapicorn/module.cc: created simple glue code example.

---
+++ pyrapicorn/__init__.py
@@ -0,0 +1,25 @@
+# Rapicorn                              -*- Mode: python; -*-
+# Copyright (C) 2008 Tim Janik
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# A copy of the GNU Lesser General Public License should ship along
+# with this library; if not, see http://www.gnu.org/copyleft/.
+
+"""Rapicorn - experimental UI toolkit
+
+More details at http://www.rapicorn.org/.
+"""
+
+#CONSTANT_X = 7
+#__all__ = ['CONSTANT_X']
+
+import pyRapicorn0752
