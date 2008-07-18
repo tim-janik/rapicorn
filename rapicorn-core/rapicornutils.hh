8b317e1f0de32985620d68b381effd03119fa921
Tim Janik <timj@gtk.org>
2008-07-19 01:41:59 +0200

    rapicornutils.hh: make Deletable::object_url() publically accessible

--- rapicorn-core/rapicornutils.hh
+++ rapicorn-core/rapicornutils.hh
@@ -342,13 +342,13 @@ struct Deletable : public virtual VirtualTypeid {
     bool         deletable_remove_hook (void      *any)              { return false; }
     bool         deletable_remove_hook (Deletable *deletable);
   };
+  String         object_url            () const;
 private:
   void           add_deletion_hook     (DeletionHook *hook);
   void           remove_deletion_hook  (DeletionHook *hook);
 protected:
   void           invoke_deletion_hooks ();
   virtual       ~Deletable             ();
-  String         object_url            () const;
   static Deletable* from_object_url    (const String &object_url);
 };
 
