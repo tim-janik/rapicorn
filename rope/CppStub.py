23844de5638cfe5457e78fd8984db060447765d1
Tim Janik <timj@gtk.org>
2010-02-26 23:22:17 +0100

    ROPE: fixed CppStub comments

--- rope/CppStub.py
+++ rope/CppStub.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# Rapicorn-PyStub                                              -*-mode:python-*-
+# Rapicorn-CppStub                                             -*-mode:python-*-
 # Copyright (C) 2009-2010 Tim Janik
 #
 # This program is free software: you can redistribute it and/or modify
@@ -14,7 +14,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
-"""Rapicorn-PyStub - Python Stub (client glue code) generator for Rapicorn
+"""Rapicorn-CppStub - C++ Stub (client glue code) generator for Rapicorn
 
 More details at http://www.rapicorn.org
 """
