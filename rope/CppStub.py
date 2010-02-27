f1628fa9d3dba71b509ccf3428162b7e41aebd99
Tim Janik <timj@gtk.org>
2010-02-28 00:18:50 +0100

    ROPE: added protocol stubs for sequence and record

--- rope/CppStub.py
+++ rope/CppStub.py
@@ -21,9 +21,45 @@ More details at http://www.rapicorn.org
 import Decls, re
 
 base_code = """
+#include <core/rapicornsignal.hh>
 #include <string>
 #include <vector>
-#include <core/rapicornsignal.hh>
+
+class ProtoRecord;
+class ProtoSequence;
+class ProtoArg;
+class ProtoMessage;
+
+// FIXME:
+#include "protocol-pb2.hh"
+typedef Rapicorn::Rope::RemoteProcedure RemoteProcedure;
+typedef Rapicorn::Rope::RemoteProcedure_Sequence RemoteProcedure_Sequence;
+typedef Rapicorn::Rope::RemoteProcedure_Record RemoteProcedure_Record;
+typedef Rapicorn::Rope::RemoteProcedure_Argument RemoteProcedure_Argument;
+static inline RemoteProcedure_Record& RPRecordCast (ProtoRecord &r) {
+  return *(RemoteProcedure_Record*) &r;
+}
+static inline const RemoteProcedure_Record& RPRecordCast (const ProtoRecord &r) {
+  return *(const RemoteProcedure_Record*) &r;
+}
+static inline ProtoRecord& ProtoRecordCast (RemoteProcedure_Record &r) {
+  return *(ProtoRecord*) &r;
+}
+static inline const ProtoRecord& ProtoRecordCast (const RemoteProcedure_Record &r) {
+  return *(const ProtoRecord*) &r;
+}
+static inline RemoteProcedure_Sequence& RPSequenceCast (ProtoSequence &r) {
+  return *(RemoteProcedure_Sequence*) &r;
+}
+static inline const RemoteProcedure_Sequence& RPSequenceCast (const ProtoSequence &r) {
+  return *(const RemoteProcedure_Sequence*) &r;
+}
+static inline ProtoSequence& ProtoSequenceCast (RemoteProcedure_Sequence &r) {
+  return *(ProtoSequence*) &r;
+}
+static inline const ProtoSequence& ProtoSequenceCast (const RemoteProcedure_Sequence &r) {
+  return *(const ProtoSequence*) &r;
+}
 
 // Base classes...
 """
@@ -89,13 +125,131 @@ class Generator:
     elif type_info.storage == Decls.SEQUENCE:
       fl = type_info.elements
       s += self.generate_field (fl[0], 'std::vector<' + self.type2cpp (fl[1].name) + '>')
+      s += '  bool to_proto   (ProtoSequence &) const;\n'
+      s += '  bool from_proto (const ProtoSequence &);\n'
     if type_info.storage == Decls.RECORD:
+      s += '  bool to_proto   (ProtoRecord &) const;\n'
+      s += '  bool from_proto (const ProtoRecord &);\n'
       s += '  inline %s () {' % type_info.name
       for fl in fieldlist:
         if fl[1].storage in (Decls.INT, Decls.FLOAT, Decls.ENUM):
           s += " %s = %s;" % (fl[0], self.mkzero (fl[1]))
       s += ' }\n'
     s += '};'
+    return s
+  def generate_to_proto (self, argname, type_info, valname):
+    s = '  %s->set_type (RemoteProcedure::%s);\n' % (argname, Decls.storage_name (type_info.storage))
+    if type_info.storage == Decls.VOID:
+      pass
+    elif type_info.storage in (Decls.INT, Decls.ENUM):
+      s += '  %s->set_vint64 (%s);\n' % (argname, valname)
+    elif type_info.storage == Decls.FLOAT:
+      s += '  %s->set_vdouble (%s);\n' % (argname, valname)
+    elif type_info.storage in (Decls.STRING, Decls.INTERFACE):
+      s += '  %s->set_vstring (%s);\n' % (argname, valname)
+    elif type_info.storage == Decls.RECORD:
+      s += '  if (!%s.to_proto (ProtoRecordCast (*%s->mutable_vrec()))) return false;\n' % (valname, argname)
+    elif type_info.storage == Decls.SEQUENCE:
+      s += '  if (!%s.to_proto (ProtoSequenceCast (*%s->mutable_vseq()))) return false;\n' % (valname, argname)
+    else: # FUNC
+      raise RuntimeError ("Unexpected storage type: " + type_info.storage)
+    return s
+  def generate_from_proto (self, argname, type_info, valname):
+    s = '  if (%s->type() != RemoteProcedure::%s) return false;\n' % (argname, Decls.storage_name (type_info.storage))
+    if type_info.storage == Decls.VOID:
+      pass
+    elif type_info.storage == Decls.INT:
+      s += '  if (!%s->has_vint64()) return false;' % argname
+      s += '  %s = %s->vint64();\n' % (valname, argname)
+    elif type_info.storage == Decls.ENUM:
+      s += '  if (!%s->has_vint64()) return false;' % argname
+      s += '  %s = %s (%s->vint64());\n' % (valname, self.type2cpp (type_info.name), argname)
+    elif type_info.storage == Decls.FLOAT:
+      s += '  if (!%s->has_vdouble()) return false;' % argname
+      s += '  %s = %s->vdouble();\n' % (valname, argname)
+    elif type_info.storage in (Decls.STRING, Decls.INTERFACE):
+      s += '  if (!%s->has_vstring()) return false;' % argname
+      s += '  %s = %s->vstring();\n' % (valname, argname)
+    elif type_info.storage == Decls.RECORD:
+      s += '  if (!%s->has_vrec() || !%s.from_proto (ProtoRecordCast (%s->vrec()))) return false;\n' % (argname, valname, argname)
+    elif type_info.storage == Decls.SEQUENCE:
+      s += '  if (!%s->has_vseq() || !%s.from_proto (ProtoSequenceCast (%s->vseq()))) return false;\n' % (argname, valname, argname)
+    else: # FUNC
+      raise RuntimeError ("Unexpected storage type: " + type_info.storage)
+    return s
+  def generate_record_proto (self, type_info):
+    s = ''
+    s += 'bool %s::to_proto (ProtoRecord &dst) const {\n' % type_info.name
+    s += '  RemoteProcedure_Record &rpr = RPRecordCast (dst);\n'
+    s += '  RemoteProcedure_Argument *field; int field_counter = 0;\n'
+    field_counter = 0
+    for fl in type_info.fields:
+      s += '  rpr.add_fields(); field = rpr.mutable_fields (field_counter++);\n'
+      s += self.generate_to_proto ('field', fl[1], fl[0])
+      field_counter += 1
+    s += '  return true;\n}\n'
+    s += 'bool %s::from_proto (const ProtoRecord &src) {\n' % type_info.name
+    s += '  const RemoteProcedure_Record &rpr = RPRecordCast (src);\n'
+    s += '  if (rpr.fields_size() < %d) return false;\n' % field_counter
+    s += '  const RemoteProcedure_Argument *field;\n'
+    field_counter = 0
+    for fl in type_info.fields:
+      s += '  field = &rpr.fields (%d);\n' % field_counter
+      s += self.generate_from_proto ('field', fl[1], fl[0])
+      field_counter += 1
+    s += '  return true;\n}\n'
+    return s
+  def storage_fieldname (self, storage):
+    if storage in (Decls.INT, Decls.ENUM):
+      return 'vint64'
+    elif storage == Decls.FLOAT:
+      return 'vdouble'
+    elif storage in (Decls.STRING, Decls.INTERFACE):
+      return 'vstring'
+    elif storage == Decls.RECORD:
+      return 'vrec'
+    elif storage == Decls.SEQ:
+      return 'vseq'
+    else: # FUNC VOID
+      raise RuntimeError ("Unexpected storage type: " + storage)
+  def generate_sequence_proto (self, type_info):
+    s = ''
+    s += 'bool %s::to_proto (ProtoSequence &dst) const {\n' % type_info.name
+    s += '  RemoteProcedure_Sequence &rps = RPSequenceCast (dst);\n'
+    el = type_info.elements
+    s += '  const size_t len = %s.size();\n' % el[0]
+    s += '  for (size_t k = 0; k < len; k++) {\n'
+    if el[1].storage in (Decls.INT, Decls.ENUM, Decls.FLOAT, Decls.STRING, Decls.INTERFACE):
+      s += '    rps.add_%s (%s[k]);\n' % (self.storage_fieldname (el[1].storage), el[0])
+    elif el[1].storage == Decls.RECORD:
+      s += '    rps.add_vrec();'
+      s += '    if (!%s[k].to_proto (ProtoRecordCast (*rps.mutable_vrec (k)))) return false;\n' % el[0]
+    elif el[1].storage == Decls.SEQUENCE:
+      s += '    rps.add_vseq();'
+      s += '    if (!%s[k].to_proto (ProtoSequenceCast (*rps.mutable_vseq (k)))) return false;\n' % el[0]
+    else: # FUNC VOID
+      raise RuntimeError ("Unexpected storage type: " + el[1].storage)
+    s += '  }\n'
+    s += '  return true;\n}\n'
+    s += 'bool %s::from_proto (const ProtoSequence &src) {\n' % type_info.name
+    s += '  const RemoteProcedure_Sequence &rps = RPSequenceCast (src);\n'
+    s += '  const size_t len = rps.%s_size();\n' % self.storage_fieldname (el[1].storage)
+    if el[1].storage in (Decls.RECORD, Decls.SEQUENCE):
+      s += '  %s.resize (len);\n' % el[0]
+    s += '  for (size_t k = 0; k < len; k++) {\n'
+    if el[1].storage in (Decls.INT, Decls.FLOAT, Decls.STRING, Decls.INTERFACE):
+      s += '    %s.push_back (rps.%s (k));\n' % (el[0], self.storage_fieldname (el[1].storage))
+    elif el[1].storage == Decls.ENUM:
+      s += '    %s.push_back (%s (rps.%s (k)));\n' % \
+           (el[0], self.type2cpp (el[1].name), self.storage_fieldname (el[1].storage))
+    elif el[1].storage == Decls.RECORD:
+      s += '    if (!%s[k].from_proto (ProtoRecordCast (rps.vrec (k)))) return false;\n' % el[0]
+    elif el[1].storage == Decls.SEQUENCE:
+      s += '    if (!%s[k].from_proto (ProtoSequenceCast (rps.vseq (k)))) return false;\n' % el[0]
+    else: # FUNC VOID
+      raise RuntimeError ("Unexpected storage type: " + el[1].storage)
+    s += '  }\n'
+    s += '  return true;\n}\n'
     return s
   def generate_signal (self, functype, ctype):
     signame = self.generate_signal_name (functype, ctype)
@@ -187,9 +341,19 @@ class Generator:
     for tp in implementation_types:
       if tp.isimpl:
         types += [ tp ]
-    # generate types
+    # generate type skeletons
+    s += '\n// --- Skeletons ---\n'
     for tp in types:
       s += self.generate_type (tp)
+    # generate type stubs
+    s += '\n// --- Stubs ---\n'
+    for tp in types:
+      if tp.typedef_origin:
+        pass
+      elif tp.storage == Decls.RECORD:
+        s += self.generate_record_proto (tp) + '\n'
+      elif tp.storage == Decls.SEQUENCE:
+        s += self.generate_sequence_proto (tp) + '\n'
     return s
 
 def error (msg):
