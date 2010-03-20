2b21bd71111f99c3c6d9f28b96a271b8af44443f
Tim Janik <timj@gtk.org>
2010-03-21 00:19:50 +0100

    UI: support custom Requisition constructor via IDL generation insertion

---
+++ ui/insertions.cc
@@ -0,0 +1,32 @@
+/* Rapicorn
+ * Copyright (C) 2010 Tim Janik
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * A copy of the GNU Lesser General Public License should ship along
+ * with this library; if not, see http://www.gnu.org/copyleft/.
+ */
+
+// PLIC insertion file
+// Here we provide insertion snippets to be included in struct
+// definitions when generating interface.hh from rapicorn.idl
+
+struct DUMMY { // dummy class for auto indentation
+
+class_scope:Requisition:
+
+  inline Requisition (double w, double h) : width (w), height (h) {}
+
+
+
+
+IGNORE: // close last _scope
+}; // close dummy class scope
