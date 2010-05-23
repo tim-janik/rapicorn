664bbda5151440ca92d61faf2c1945347b1bb285
Tim Janik <timj@gtk.org>
2010-05-23 22:15:54 +0200

    ROPE: renamed py-rope

--- rope/cxx-rope.cc
+++ rope/cxx-rope.cc
@@ -14,7 +14,7 @@
  * A copy of the GNU Lesser General Public License should ship along
  * with this library; if not, see http://www.gnu.org/copyleft/.
  */
-#include "rope.hh" // must be included first to configure std headers
+#include "py-rope.hh" // must be included first to configure std headers
 using namespace Rapicorn;
 
 #define HAVE_PLIC_CALL_REMOTE   1
