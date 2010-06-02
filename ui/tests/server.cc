6878f2fed454f5fc7d6b189e081965de18a73645
Tim Janik <timj@gtk.org>
2010-06-02 03:44:11 +0200

    UI: added first server API test

---
+++ ui/tests/server.cc
@@ -0,0 +1,46 @@
+/* Rapicorn
+ * Copyright (C) 2010 Tim Janik
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * A copy of the GNU Lesser General Public License should ship along
+ * with this library; if not, see http://www.gnu.org/copyleft/.
+ */
+#include <rcore/testutils.hh>
+#include <rapicorn.hh>
+using namespace Rapicorn;
+
+static Application * volatile smart_app_p = NULL;
+
+static void
+test_smart_handle (void)
+{
+  ApplicationBase *ab = &App;
+  Plic::FieldBuffer8 fb (4);
+  fb.add_object (uint64 ((BaseObject*) ab));
+  Plic::CallContext c;
+  Plic::FieldBufferReader fbr (fb);
+  Application sh (c, fbr);
+  ApplicationBase &shab = *sh;
+  assert (ab == &shab);
+  assert (ab == sh.operator->());
+}
+
+int
+main (int   argc,
+      char *argv[])
+{
+  rapicorn_init_test (&argc, &argv);
+
+  Test::add ("Server/Smart Handle", test_smart_handle);
+
+  return Test::run();
+}
