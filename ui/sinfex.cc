eff8e57869348283f21c7323af0850ff7f63329e
Tim Janik <timj@gtk.org>
2010-06-09 19:35:25 +0200

    UI: fixed size_t printf format for AMD64

--- ui/sinfex.cc
+++ ui/sinfex.cc
@@ -351,7 +351,7 @@ SinfexExpression::eval_op (Scope &scope,
       }
     case SINFEX_ARG: ;
     }
-  error ("Expression with invalid op code: (%p:%u:%u)", m_start, mark.up - 1 - m_start, *(mark.up-1));
+  error ("Expression with invalid op code: (%p:%zu:%u)", m_start, mark.up - 1 - m_start, *(mark.up-1));
 }
 
 /* --- Parser Class --- */
