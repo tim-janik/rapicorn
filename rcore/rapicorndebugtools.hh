25577673f1ef41bca2e2b607cdfff09acb9950ea
Tim Janik <timj@gtk.org>
2011-04-30 23:23:09 +0200

    RCORE: removed unused DebugChannel implementation

--- rcore/rapicorndebugtools.hh
+++ rcore/rapicorndebugtools.hh
@@ -1,49 +1,14 @@
-/* Rapicorn
- * Copyright (C) 2007 Tim Janik
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * A copy of the GNU Lesser General Public License should ship along
- * with this library; if not, see http://www.gnu.org/copyleft/.
+/* Copyright (C) 2009 Tim Janik
+ * GNU LGPL v3 or later: http://www.gnu.org/licenses/lgpl.html
  */
 #ifndef __RAPICORN_DEBUG_TOOLS_HH__
 #define __RAPICORN_DEBUG_TOOLS_HH__
 
 #include <rcore/rapicornutils.hh>
-#include <stdarg.h>
 
 namespace Rapicorn {
 
-class DebugChannel : public virtual ReferenceCountable, protected NonCopyable {
-protected:
-  explicit              DebugChannel        ();
-  virtual               ~DebugChannel       ();
-public:
-  virtual void          printf_valist       (const char *format,
-                                             va_list     args) = 0;
-  inline void           printf              (const char *format, ...) RAPICORN_PRINTF (2, 3);
-  static DebugChannel*  new_from_file_async (const String &filename);
-};
-
-inline void
-DebugChannel::printf (const char *format,
-                      ...)
-{
-  va_list a;
-  va_start (a, format);
-  printf_valist (format, a);
-  va_end (a);
-}
-
-/* test dump */
+/* --- test dump --- */
 class TestStream : protected NonCopyable {
 public:
   typedef enum { TEXT, NODE, VALUE, INTERN, INDENT, POPNODE, POPINDENT } Kind;
