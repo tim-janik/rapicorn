e8ca60d8f5d1dec5228e155449ea3d6eff5932ca
Tim Janik <timj@gtk.org>
2011-09-21 03:45:27 +0200

    UI: use TCMP() for region tests where possible

--- ui/tests/region.cc
+++ ui/tests/region.cc
@@ -15,11 +15,11 @@ test_rect (void)
   r1.assign (Point (7.1, 7.2), Point (8.4, 8.5));       // stores width=1.3, height=1.3 in double precision
   TASSERT (!r1.empty());
   Rect r2 (7.1, 7.2, 1.3, 1.3);
-  TASSERT (r2 == Rect (7.1, 7.2, 1.3, 1.3));
+  TCMP (r2, ==, Rect (7.1, 7.2, 1.3, 1.3));
   TASSERT (false == (r2 != Rect (7.1, 7.2, 1.3, 1.3)));
   Rect r3 (7.1, 7.2, 1.0, 1.0);
-  TASSERT (r3.equals (r2, 0.1) == false);
-  TASSERT (r3.equals (r2, 0.5) == true);
+  TCMP (r3.equals (r2, 0.1), ==, false);
+  TCMP (r3.equals (r2, 0.5), ==, true);
   TASSERT (r2.equals (r1, 0.00000000000001));           // using epsilon due to precision artefacts
 }
 REGISTER_UITHREAD_TEST ("Region/rectangles", test_rect);
@@ -53,22 +53,22 @@ test_region_rect1 (void)
   Region r;
   r.clear();
   TASSERT (r.empty());
-  TASSERT (r.contains (Point (0, 0)) == false);
+  TCMP (r.contains (Point (0, 0)), ==, false);
   r.add (Rect (Point (3, 3), Point (5, 5)));
   TASSERT (!r.empty());
-  TASSERT (r.contains (2, 2) == false);
+  TCMP (r.contains (2, 2), ==, false);
   TASSERT (r.contains (3, 3));
   TASSERT (r.contains (4, 4));
-  TASSERT (r.contains (5, 5) == false);
-  TASSERT (r.contains (6, 6) == false);
-  TASSERT (r.contains (Rect (Point (3, 3), Point (5, 5))) == r.INSIDE);
-  TASSERT (r.contains (Rect (Point (4, 4), Point (6, 6))) == r.PARTIAL);
-  TASSERT (r.contains (Rect (Point (5, 5), Point (6, 6))) == r.OUTSIDE);
+  TCMP (r.contains (5, 5), ==, false);
+  TCMP (r.contains (6, 6), ==, false);
+  TCMP (r.contains (Rect (Point (3, 3), Point (5, 5))), ==, r.INSIDE);
+  TCMP (r.contains (Rect (Point (4, 4), Point (6, 6))), ==, r.PARTIAL);
+  TCMP (r.contains (Rect (Point (5, 5), Point (6, 6))), ==, r.OUTSIDE);
   vector<Rect> rects;
   r.list_rects (rects);
-  TASSERT (rects.size() == 1);
-  TASSERT (rects[0].lower_left() == Point (3, 3));
-  TASSERT (rects[0].upper_right() == Point (5, 5));
+  TCMP (rects.size(), ==, 1);
+  TCMP (rects[0].lower_left(), ==, Point (3, 3));
+  TCMP (rects[0].upper_right(), ==, Point (5, 5));
   Region z (r);
   TASSERT (r.equal (z));
   r.exor (r);
@@ -76,12 +76,12 @@ test_region_rect1 (void)
   TASSERT (!r.equal (z));
   z.clear();
   TASSERT (r.equal (z));
-  TASSERT (r.extents() == Rect (Point (0, 0), Point (0, 0)));
+  TCMP (r.extents(), ==, Rect (Point (0, 0), Point (0, 0)));
   const Region e;
-  TASSERT (e.contains (Point (0, 0)) == false);
-  TASSERT (e.contains (Rect (Point (0, 0), Point (0, 0))) == true);
-  TASSERT (e.contains (Rect (Point (0, 0), Point (1, 1))) == false);
-  TASSERT (e.contains (Region()) == true);
+  TCMP (e.contains (Point (0, 0)), ==, false);
+  TCMP (e.contains (Rect (Point (0, 0), Point (0, 0))), ==, true);
+  TCMP (e.contains (Rect (Point (0, 0), Point (1, 1))), ==, false);
+  TCMP (e.contains (Region()), ==, true);
 }
 REGISTER_UITHREAD_TEST ("Region/region rec1t", test_region_rect1);
 
@@ -90,24 +90,24 @@ test_region2 (void)
 {
   Region a (Rect (Point (-1, -1), Point (1, 1)));
   Region b (Rect (Point (-1, -1), Point (0, 0)));
-  TASSERT (a.equal (b) == false);
+  TCMP (a.equal (b), ==, false);
   TASSERT ((a != b) == true);
   TASSERT ((a == b) == false);
-  TASSERT (a.cmp (b) != 0);
+  TCMP (a.cmp (b), !=, 0);
   if (a.cmp (b) < 0)
-    TASSERT (a < b);
+    TCMP (a, <, b);
   else
-    TASSERT (b < a);
+    TCMP (b, <, a);
   a.clear();
   b.clear();
   TASSERT (a.empty());
   TASSERT (b.empty());
-  TASSERT (a.equal (b) == true);
+  TCMP (a.equal (b), ==, true);
   TASSERT ((a != b) == false);
   TASSERT ((a == b) == true);
-  TASSERT (a.cmp (b) == 0);
-  TASSERT ((a < b) == false);
-  TASSERT ((b < a) == false);
+  TCMP (a.cmp (b), ==, 0);
+  TCMP ((a < b), ==, false);
+  TCMP ((b < a), ==, false);
 }
 REGISTER_UITHREAD_TEST ("Region/region2", test_region2);
 
@@ -118,34 +118,35 @@ test_region2_ops (void)
   Region b (Rect (Point (-1, -1), Point (0, 0))), sb = b;
   Region c (Rect (Point (-1, -1), Point (0, 0))), sc = c;
   b.intersect (a);
-  TASSERT (b == c);
-  TASSERT (a != c);
-  TASSERT (b == sb);
-  TASSERT (a == sa);
+  TCMP (b, ==, c);
+  TCMP (a, !=, c);
+  TCMP (b, ==, sb);
+  TCMP (a, ==, sa);
   a.intersect (b);
-  TASSERT (a != sa);
-  TASSERT (a == c);
+  TCMP (a, !=, sa);
+  TCMP (a, ==, c);
   a = sa;
-  TASSERT (a == sa);
-  TASSERT (a.contains (c.extents()) == true);
+  TCMP (a, ==, sa);
+  TCMP (a.contains (c.extents()), ==, true);
   a.subtract (b);
-  TASSERT (a != sa);
-  TASSERT (a.contains (c.extents()) == false);
+  TCMP (a, !=, sa);
+  TCMP (a.contains (c.extents()), ==, false);
   a.add (b);
-  TASSERT (a.contains (c.extents()) == true);
-  TASSERT (a == sa);
+  TCMP (a.contains (c.extents()), ==, true);
+  TCMP (a, ==, sa);
   a.add (b);
-  TASSERT (a == sa);
+  TCMP (a, ==, sa);
   a.intersect (a);
-  TASSERT (a == sa);
+  TCMP (a, ==, sa);
   a.exor (a);
   TASSERT (a.empty());
   a.exor (b);
-  TASSERT (a == b);
+  TCMP (a, ==, b);
   b.exor (a);
-  TASSERT (b.empty() && b != sb);
+  TASSERT (b.empty());
+  TCMP (b, !=, sb);
   b.exor (a);
-  TASSERT (b == sb);
+  TCMP (b, ==, sb);
 }
 REGISTER_UITHREAD_TEST ("Region/region2 ops", test_region2_ops);
 
@@ -168,33 +169,33 @@ test_region_fract (void)
   Region r;
   TASSERT (r.empty());
   const double epsilon = r.epsilon();   /* allow errors within +-epsilon */
-  TASSERT (epsilon <= 0.5);             /* assert at least pxiel resolution */
+  TCMP (epsilon, <=, 0.5);             /* assert at least pxiel resolution */
   r.clear();
   TASSERT (r.empty());
-  TASSERT (r.contains (Point (0, 0)) == false);
+  TCMP (r.contains (Point (0, 0)), ==, false);
   r.add (Rect (Point (0.1, 0.2), Point (3.4, 4.5)));
   TASSERT (!r.empty());
   TASSERT (r.contains (2, 2));
-  TASSERT (r.contains (0, 0) == false);
+  TCMP (r.contains (0, 0), ==, false);
   TASSERT (r.contains (1, 1));
   TASSERT (r.contains (3, 3));
   TASSERT (r.contains (3.3, 3));
-  TASSERT (r.contains (3.5, 3) == false);
+  TCMP (r.contains (3.5, 3), ==, false);
   TASSERT (r.contains (3.3, 4.4));
-  TASSERT (r.contains (3.3, 4.6) == false);
-  TASSERT (r.contains (6, 6) == false);
-  TASSERT (r.contains (Rect (1, 1, 2, 2)) == r.INSIDE);
-  TASSERT (r.contains (Rect (0, 0, 3, 3)) == r.PARTIAL);
-  TASSERT (r.contains (Rect (3.5, 0, 70, 70)) == r.OUTSIDE);
+  TCMP (r.contains (3.3, 4.6), ==, false);
+  TCMP (r.contains (6, 6), ==, false);
+  TCMP (r.contains (Rect (1, 1, 2, 2)), ==, r.INSIDE);
+  TCMP (r.contains (Rect (0, 0, 3, 3)), ==, r.PARTIAL);
+  TCMP (r.contains (Rect (3.5, 0, 70, 70)), ==, r.OUTSIDE);
   Region r2 = r;
   TASSERT (r2.extents().equals (Rect (0.1, 0.2, 3.3, 4.3), epsilon));
   r2.add (Rect (0, 0, 0.1, 4.5));
   r2.add (Rect (0, 0, 3.4, 0.2));
   Region r3 (Rect (0, 0, 3.4, 4.5));
-  TASSERT (r2 == r3);
+  TCMP (r2, ==, r3);
   std::vector<Rect> rects;
   r3.list_rects (rects);
-  TASSERT (rects.size() == 1);
+  TCMP (rects.size(), ==, 1);
   TASSERT (rects[0].equals (Rect (0, 0, 3.4, 4.5), epsilon));
   if (0)
     printf ("\nREGION:\n%s\nEMPTY:\n%s\n", create_rand_region().string().c_str(), Region().string().c_str());
@@ -208,61 +209,61 @@ test_region_cmp ()
   for (uint i = 0; i < 65 || cases != 7; i++)
     {
       Region r1 = create_rand_region();
-      TCHECK (r1.contains (r1) == r1.INSIDE);
-      TCHECK (r1.contains (Region (r1)) == r1.INSIDE);
+      TCMP (r1.contains (r1), ==, r1.INSIDE);
+      TCMP (r1.contains (Region (r1)), ==, r1.INSIDE);
       Region r2 = create_rand_region();
-      TCHECK (r2.contains (r2) == r2.INSIDE);
-      TCHECK (r2.contains (Region (r2)) == r2.INSIDE);
+      TCMP (r2.contains (r2), ==, r2.INSIDE);
+      TCMP (r2.contains (Region (r2)), ==, r2.INSIDE);
       int c1 = r1.cmp (r2);
       int c2 = r2.cmp (r1);
-      TCHECK (c1 == -c2);
+      TCMP (c1, ==, -c2);
       if (c1)
         {
-          TCHECK (r1 != r2);
-          TCHECK (r2 != r1);
+          TCMP (r1, !=, r2);
+          TCMP (r2, !=, r1);
         }
       else
         {
-          TCHECK (r1 == r2);
-          TCHECK (r2 == r1);
+          TCMP (r1, ==, r2);
+          TCMP (r2, ==, r1);
         }
       if (c1 < 0)
         {
-          TCHECK (r1 < r2);
-          TCHECK (r1.equal (r2) == false);
-          TCHECK ((r2 < r1) == false);
-          TCHECK (r2.equal (r1) == false);
+          TCMP (r1, <, r2);
+          TCMP (r1.equal (r2), ==, false);
+          TCMP ((r2 < r1), ==, false);
+          TCMP (r2.equal (r1), ==, false);
           cases |= 1;
         }
       else if (c1 == 0)
         {
-          TCHECK ((r1 < r2) == false);
-          TCHECK (r1.equal (r2) == true);
-          TCHECK ((r2 < r1) == false);
-          TCHECK (r2.equal (r1) == true);
+          TCMP ((r1 < r2), ==, false);
+          TCMP (r1.equal (r2), ==, true);
+          TCMP ((r2 < r1), ==, false);
+          TCMP (r2.equal (r1), ==, true);
           cases |= 2;
         }
       else if (c1 > 0)
         {
-          TCHECK (r2 < r1);
-          TCHECK (r2.equal (r1) == false);
-          TCHECK ((r1 < r2) == false);
-          TCHECK (r1.equal (r2) == false);
+          TCMP (r2, <, r1);
+          TCMP (r2.equal (r1), ==, false);
+          TCMP ((r1 < r2), ==, false);
+          TCMP (r1.equal (r2), ==, false);
           cases |= 4;
         }
       Region c;
-      TCHECK (r1.contains (c)); // empty containment
-      TCHECK (r2.contains (c)); // empty containment
+      TASSERT (r1.contains (c)); // empty containment
+      TASSERT (r2.contains (c)); // empty containment
       c.add (r1);
       c.add (r2);
-      TCHECK (c.contains (r1));
-      TCHECK (c.contains (r2));
+      TASSERT (c.contains (r1));
+      TASSERT (c.contains (r2));
       c.subtract (r1);
       c.subtract (r2);
       if (!r1.empty())
-        TCHECK (c.contains (r1) == false);
+        TCMP (c.contains (r1), ==, false);
       if (!r2.empty())
-        TCHECK (c.contains (r2) == false);
+        TCMP (c.contains (r2), ==, false);
       TASSERT (c.empty());
     }
 }
