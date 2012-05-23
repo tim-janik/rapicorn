0cb2e454200bc089b0a678d2d579a085373e25ff
Tim Janik <timj@gnu.org>
2012-05-23 21:14:26 +0200

    RCORE: provide brief doc for Rapicorn namespace

--- rcore/rcore.hh
+++ rcore/rcore.hh
@@ -1,19 +1,4 @@
-/* Rapicorn
- * Copyright (C) 2006 Tim Janik
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * A copy of the GNU Lesser General Public License should ship along
- * with this library; if not, see http://www.gnu.org/copyleft/.
- */
+// Licensed GNU LGPL v3 or later: http://www.gnu.org/licenses/lgpl.html
 #ifndef __RAPICORN_CORE_HH__
 #define __RAPICORN_CORE_HH__
 
@@ -38,12 +23,11 @@
 
 /**
  * @namespace Rapicorn
- * The Rapicorn namespace encompasses core utilities and toolkit
- * functionality. The core utilities are available via including
- * <rapicorn-core.hh> and the toolkit functionality can be included
- * as <rapicorn.hh>.
+ * @brief The Rapicorn namespace encompasses core utilities and toolkit functionality.
+ *
+ * The core utilities are available via including <rapicorn-core.hh> and
+ * the toolkit functionality can be included via <rapicorn.hh>.
  */
 namespace Rapicorn {}
 
 #endif // __RAPICORN_CORE_HH__
-/* vim:set ts=8 sts=2 sw=2: */
