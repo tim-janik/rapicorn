5f89995199563ef8aca681066297cc000c975f29
Tim Janik <timj@gnu.org>
2012-09-18 13:19:25 +0200

    RCORE: comment fixups

--- rcore/serverinsert.hh
+++ rcore/serverinsert.hh
@@ -1,6 +1,6 @@
 // Licensed GNU LGPL v3 or later: http://www.gnu.org/licenses/lgpl.html
 
-// PLIC insertion file
+// C++ interface code insertions for server stubs
 
 includes:
 #include <rcore/rapicornconfig.h>
