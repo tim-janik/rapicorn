cfc0c9b4fb111e8c1a54a0593e7b21d8a9f2a66e
Tim Janik <timj@gnu.org>
2012-10-29 04:05:37 +0100

    ROPE: license cleanups

--- rope/PyStub.py
+++ rope/PyStub.py
@@ -1,19 +1,5 @@
 #!/usr/bin/env python
-# Rapicorn-PyStub                                              -*-mode:python-*-
-# Copyright (C) 2009-2010 Tim Janik
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program. If not, see <http://www.gnu.org/licenses/>.
+# Licensed GNU LGPL v3 or later: http://www.gnu.org/licenses/lgpl.html
 """Rapicorn-PyStub - Python Stub (client glue code) generator for Rapicorn
 
 More details at http://www.rapicorn.org
