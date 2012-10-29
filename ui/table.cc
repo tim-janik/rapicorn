be8d039d6ec162dcc9f2b6cc44be883b46dcbeef
Tim Janik <timj@gnu.org>
2012-10-29 03:51:46 +0100

    UI: license cleanups

--- ui/table.cc
+++ ui/table.cc
@@ -1,23 +1,6 @@
-/* Rapicorn
- * Copyright (C) 2005 Tim Janik
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * A copy of the GNU Lesser General Public License should ship along
- * with this library; if not, see http://www.gnu.org/copyleft/.
- *
- * - This file is derived from GtkTable code which is
- *   Copyright (C) 1996-2002 by the GTK+ project.
- */
+// Licensed GNU LGPL v3 or later: http://www.gnu.org/licenses/lgpl.html
 #include "tableimpl.hh"
+// This file is derived from GtkTable code, Copyright (C) 1996-2002 by the GTK+ project.
 #include "factory.hh"
 
 #include <algorithm>
