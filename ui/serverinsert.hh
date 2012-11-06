cbb4cdbed4923ba73533f08308e28b887f5ece8c
Tim Janik <timj@gnu.org>
2012-11-06 03:42:50 +0100

    UI: moved Pixbuf here

--- ui/serverinsert.hh
+++ ui/serverinsert.hh
@@ -13,6 +13,20 @@ class WindowImpl;
 IGNORE:
 struct DUMMY { // dummy class for auto indentation
 
+class_scope:Pixbuf:
+  /// Construct Pixbuf at given width and height.
+  explicit        PixbufImpl   (uint w, uint h) : row_length (0) { pixels.resize (w, h); }
+  /// Reset width and height and resize pixel sequence.
+  void            resize       (uint w, uint h) { row_length = w; pixels.resize (row_length * h); }
+  /// Access row as endian dependant ARGB integers.
+  uint32_t*       row          (uint y)         { return y < uint32_t (height()) ? (uint32_t*) &pixels[row_length * y] : NULL; }
+  /// Access row as endian dependant ARGB integers.
+  const uint32_t* row          (uint y) const   { return y < uint32_t (height()) ? (uint32_t*) &pixels[row_length * y] : NULL; }
+  /// Width of the Pixbuf.
+  int             width        () const         { return row_length; }
+  /// Height of the Pixbuf.
+  int             height       () const         { return row_length ? pixels.size() / row_length : 0; }
+
 class_scope:Requisition:
   inline RequisitionImpl (double w, double h) : width (w), height (h) {}
 
