d32373beb8fab97e5fe42da08b83a4f329f61828
Tim Janik <timj@gnu.org>
2013-03-13 23:56:53 +0100

    PYTESTS: add simple numeric property test

--- pytests/testrapicorn.py
+++ pytests/testrapicorn.py
@@ -28,8 +28,8 @@ window.name = "frob17"  ; assert window.name == "frob17"        # testing derive
 window.name = "window1" ; assert window.name == "window1"
 window.title = "foo"    ; assert window.title == "foo"          # testing narrowed property
 window.title = "bar"    ; assert window.title == "bar"
-
-
+for i in (9999, 22, 778, 0, 1, 2, 3, 123):
+  window.width = i ; assert window.width == i
 
 # print positive feedback if we made it here
 print " " * max (0, 75 - len ("  " + __file__)), "OK"
