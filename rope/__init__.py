141d53d0e41905c1f00974e56bb3bc016bd85508
Tim Janik <timj@gnu.org>
2013-03-14 18:31:44 +0100

    ROPE: derive MainApplication from Rapicorn.Application for proper event loop integration

--- rope/__init__.py
+++ rope/__init__.py
@@ -5,7 +5,41 @@
 More details at http://www.rapicorn.org/.
 """
 
-from Aida1208 import loop as _loop
+from Aida1208 import loop as _loop      # Aida helper module
+import __pyrapicorn                     # Generated C++ methods
+from pyrapicorn import *                # Generated Python API
+
+class MainApplication (Application):
+  def __init__ (self, _aida_id):
+    super (MainApplication, self).__init__ (_aida_id)
+    self.__dict__['__cached_exitable'] = False
+    self.sig_missing_primary += lambda: app.__dict__.__setitem__ ('__cached_exitable', True)
+  def iterate (self, may_block, may_dispatch):
+    delete_loop = None
+    try:
+      event_loop = self.__dict__['__aida_event_loop__']
+    except KeyError:
+      event_loop = _loop.Loop()
+      event_loop += _loop.RapicornSource()
+      delete_loop = event_loop
+    needs_dispatch = event_loop.iterate (may_block, may_dispatch)
+    event_loop = None
+    del delete_loop
+    return needs_dispatch
+  def loop (self):
+    assert not self.__dict__.has_key ('__aida_event_loop__')
+    event_loop = _loop.Loop()
+    event_loop += _loop.RapicornSource()
+    self.__dict__['__aida_event_loop__'] = event_loop
+    while event_loop.quit_status == None:
+      if not event_loop.iterate (False, True):                  # complete events already queued
+        break
+    self.__dict__['__cached_exitable'] = app.finishable()       # run while not exitable
+    while not self.__dict__['__cached_exitable'] and event_loop.quit_status == None:
+      self.iterate (True, True)                                 # also processes signals
+    del self.__dict__['__aida_event_loop__']
+    exit_status = event_loop.quit_status
+    return exit_status
 
 app = None
 def app_init (application_name = None):
@@ -39,51 +73,18 @@ def app_init (application_name = None):
       return True
   _loop.RapicornSource = RapicornSource
   # setup global Application
-  app = Application (_PY._BaseClass_._AidaID_ (aida_id))
-  app.__dict__['cached_primary'] = True
-  app.__dict__['primary_sig'] = 0
-  def iterate (self, may_block, may_dispatch):
-    if hasattr (self, "__aida_event_loop__"):
-      loop = self.__aida_event_loop__
-      dloop = None
-    else:
-      dloop = _loop.Loop()
-      loop = dloop
-      loop += _loop.RapicornSource()
-    needs_dispatch = loop.iterate (may_block, may_dispatch)
-    loop = None
-    del dloop
-    return needs_dispatch
-  app.__class__.iterate = iterate # extend for event loop integration
-  def loop (self):
-    event_loop = _loop.Loop()
-    event_loop += _loop.RapicornSource()
-    if not self.__dict__['primary_sig']:
-      self.__dict__['primary_sig'] = app.sig_missing_primary.connect (lambda: setattr (app.__dict__, 'cached_primary', 0))
-    self.__dict__['__aida_event_loop__'] = event_loop
-    while event_loop.quit_status == None:
-      if not event_loop.iterate (False, True):  # handle queued events
-        break
-    self.__dict__['cached_primary'] = not app.finishable()  # run while not exitable
-    while self.__dict__['cached_primary'] and event_loop.quit_status == None:
-      self.iterate (True, True)
-    del self.__dict__['__aida_event_loop__']
-    exit_status = event_loop.quit_status
-    return exit_status
-  app.__class__.loop = loop # extend for event loop integration
-  _loop.app = app # integrate event loop with app
+  app = MainApplication (_PY._BaseClass_._AidaID_ (aida_id))
+  _loop.app = app # integrate event loop with app FIXME
   return app
 
 def _module_init_once_():
   global _module_init_once_ ; del _module_init_once_
-  import __pyrapicorn   # generated C++ methods
   import pyrapicorn     # generated Python classes
   __pyrapicorn.__AIDA_BaseRecord__ = pyrapicorn._BaseRecord_ # FIXME
   pyrapicorn._CPY = __pyrapicorn # FIXME
   app_init._CPY, app_init._PY = (__pyrapicorn, pyrapicorn) # app_init() internals
-  del globals()['__pyrapicorn']
   del globals()['pyrapicorn']
 _module_init_once_()
 
 # introduce module symbols
-from pyrapicorn import *
+del globals()['__pyrapicorn']
