fc8742a50766ed65dcdc0ea81220ad2014a94bc4
Tim Janik <timj@gnu.org>
2014-10-09 06:45:45 +0200

    BUILD: include intltoolize scripts in dist tarballs

--- autogen.sh
+++ autogen.sh
@@ -6,11 +6,16 @@ echo "$0: Cleaning configure caches..."
 rm -rf autom4te.cache/
 rm -f  config.cache
 
-echo "$0: Enforce ChangeLog presence"	# automake *requires* ChangeLog
+# automake *requires* ChangeLog
+echo "$0: Enforce ChangeLog presence"
 test -e ChangeLog || TZ=GMT0 touch ChangeLog -t 190112132145.52
 
 echo "$0: autoreconf -vfsi -Wno-portability"
 autoreconf -vfsi -Wno-portability || exit $?
+
+# intltool-extract.in intltool-merge.in intltool-update.in po/Makefile.in.in
+echo "$0: intltoolize --force --automake"
+intltoolize --force --automake
 
 echo "$0: Override po/Makefile.in.in with po/Makefile.intltool"
 rm -f po/Makefile.in.in && cp -v po/Makefile.intltool po/Makefile.in.in || exit $?
