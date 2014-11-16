520653dff58f7c39660c34a9402be58e15bf6a18
Tim Janik <timj@gnu.org>
2014-11-16 05:06:52 +0100

    RSVG: add namespace in "Rapicorn___RsvgHandle" to avoid GType name clashes

--- rcore/rsvg/rsvg-gobject.c
+++ rcore/rsvg/rsvg-gobject.c
@@ -60,7 +60,13 @@ enum {
 extern double rsvg_internal_dpi_x;
 extern double rsvg_internal_dpi_y;
 
-G_DEFINE_TYPE (RsvgHandle, rsvg_handle, G_TYPE_OBJECT)
+// G_DEFINE_TYPE (RsvgHandle, rsvg_handle, G_TYPE_OBJECT)
+// Extract G_DEFINE_TYPE(RsvgHandle...) and prefix the typename with "Rapicorn___"
+// to avoid global name collisions in the GType type system.
+// G_DEFINE_TYPE(RsvgHandle...) EXTRACT-START
+static void rsvg_handle_init (RsvgHandle *self); static void rsvg_handle_class_init (RsvgHandleClass *klass); static gpointer rsvg_handle_parent_class = ((void *)0); static gint RsvgHandle_private_offset; static void rsvg_handle_class_intern_init (gpointer klass) { rsvg_handle_parent_class = g_type_class_peek_parent (klass); if (RsvgHandle_private_offset != 0) g_type_class_adjust_private_offset (klass, &RsvgHandle_private_offset); rsvg_handle_class_init ((RsvgHandleClass*) klass); } __attribute__((__unused__)) static inline gpointer rsvg_handle_get_instance_private (RsvgHandle *self) { return (((gpointer) ((guint8*) (self) + (glong) (RsvgHandle_private_offset)))); } GType rsvg_handle_get_type (void) { static volatile gsize g_define_type_id__volatile = 0; if ((__extension__ ({ typedef char _GStaticAssertCompileTimeAssertion_0[(sizeof *(&g_define_type_id__volatile) == sizeof (gpointer)) ? 1 : -1] __attribute__((__unused__)); (void) (0 ? (gpointer) *(&g_define_type_id__volatile) : 0); (!(__extension__ ({ typedef char _GStaticAssertCompileTimeAssertion_1[(sizeof *(&g_define_type_id__volatile) == sizeof (gpointer)) ? 1 : -1] __attribute__((__unused__)); __sync_synchronize (); (gpointer) *(&g_define_type_id__volatile); })) && g_once_init_enter (&g_define_type_id__volatile)); }))) { GType g_define_type_id = g_type_register_static_simple (((GType) ((20) << (2))), g_intern_static_string (
+"Rapicorn___RsvgHandle"), sizeof (RsvgHandleClass), (GClassInitFunc) rsvg_handle_class_intern_init, sizeof (RsvgHandle), (GInstanceInitFunc) rsvg_handle_init, (GTypeFlags) 0); { {{};} } (__extension__ ({ typedef char _GStaticAssertCompileTimeAssertion_2[(sizeof *(&g_define_type_id__volatile) == sizeof (gpointer)) ? 1 : -1] __attribute__((__unused__)); (void) (0 ? *(&g_define_type_id__volatile) = (g_define_type_id) : 0); g_once_init_leave ((&g_define_type_id__volatile), (gsize) (g_define_type_id)); })); } return g_define_type_id__volatile; }
+// G_DEFINE_TYPE(RsvgHandle...) EXTRACT-DONE
 
 static void
 rsvg_handle_init (RsvgHandle * self)
