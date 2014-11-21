a4af91fdffb7ba12f16b678f5c36634e199901b3
Tim Janik <timj@gnu.org>
2014-11-22 00:39:17 +0100

    UI: add StatePainter to display state dependent SVG image elements

--- ui/image.cc
+++ ui/image.cc
@@ -173,4 +173,91 @@ get_broken16_pixdata (void)
   return broken16_pixdata;
 }
 
+
+// == StatePainterImpl ==
+void
+StatePainterImpl::source (const String &resource)
+{
+  return_unless (source_ != resource);
+  source_ = resource;
+  element_image_ = NULL;
+  state_image_ = NULL;
+  state_element_ = "";
+  invalidate();
+  changed ("source");
+}
+
+void
+StatePainterImpl::element (const String &e)
+{
+  return_unless (element_ != e);
+  element_ = e;
+  element_image_ = NULL;
+  state_image_ = NULL;
+  state_element_ = "";
+  invalidate();
+  changed ("element");
+}
+
+String
+StatePainterImpl::current_element ()
+{
+  StateType s = ancestry_impressed() ? STATE_IMPRESSED : state(); // FIXME: priority for insensitive?
+  switch (s)
+    {
+    case STATE_NORMAL:          return normal_element_.empty()      ? element_ : normal_element_;
+    case STATE_INSENSITIVE:     return insensitive_element_.empty() ? element_ : insensitive_element_;
+    case STATE_PRELIGHT:        return prelight_element_.empty()    ? element_ : prelight_element_;
+    case STATE_IMPRESSED:       return impressed_element_.empty()   ? element_ : impressed_element_;
+    case STATE_FOCUS:           return focus_element_.empty()       ? element_ : focus_element_;
+    case STATE_DEFAULT:         return default_element_.empty()     ? element_ : default_element_;
+    default:                    return element_;
+    }
+}
+
+void
+StatePainterImpl::update_element (String &member, const String &value, const char *name)
+{
+  return_unless (member != value);
+  const String previous = current_element();
+  member = value;
+  if (previous != current_element())
+    invalidate (INVALID_CONTENT);
+  changed (name);
+}
+
+void
+StatePainterImpl::size_request (Requisition &requisition)
+{
+  if (!element_image_)
+    element_image_ = load_source (source_, element_);
+  get_image_size (element_image_, requisition);
+}
+
+void
+StatePainterImpl::size_allocate (Allocation area, bool changed)
+{} // default: allocation_ = area;
+
+void
+StatePainterImpl::do_changed (const String &name)
+{
+  ImageRendererImpl::do_changed (name);
+  if (name == "state" && state_element_ != current_element())
+    invalidate (INVALID_CONTENT);
+}
+
+void
+StatePainterImpl::render (RenderContext &rcontext, const Rect &rect)
+{
+  const String current = current_element();
+  if (!state_image_ || state_element_ != current)
+    {
+      state_image_ = load_source (source_, current);
+      state_element_ = state_image_ ? current : "";
+    }
+  paint_image (state_image_, rcontext, rect);
+}
+
+static const WidgetFactory<StatePainterImpl> state_painter_factory ("Rapicorn_Factory:StatePainter");
+
 } // Rapicorn
