aaecf90274f3137c4f55e5adfbb130bafb0afdf8
Tim Janik <timj@gnu.org>
2014-12-08 05:43:10 +0100

    UI: screenwindow-x11: fix subtle use-after-free for XClassHint fields

--- ui/displaywindow-x11.cc
+++ ui/displaywindow-x11.cc
@@ -1137,7 +1137,8 @@ DisplayWindowX11::setup_window (const DisplayWindow::Setup &setup)
   if (f & (HIDDEN | ICONIFY))
     wmhints.initial_state = IconicState;
   const char *cmdv[2] = { program_file().c_str(), NULL };
-  XClassHint class_hint = { const_cast<char*> (program_alias().c_str()), const_cast<char*> (program_ident().c_str()) };
+  const String prgalias = program_alias(), prgident = program_ident(); // keep strings alive until after Xutf8SetWMProperties
+  XClassHint class_hint = { const_cast<char*> (prgalias.c_str()), const_cast<char*> (prgident.c_str()) };
   Xutf8SetWMProperties (x11context.display, window_, NULL, NULL, const_cast<char**> (cmdv), ARRAY_SIZE (cmdv) - 1,
                         NULL, &wmhints, &class_hint);
   // _MOTIF_WM_HINTS
