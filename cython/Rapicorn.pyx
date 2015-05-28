eb25991c7cc2a55378663afd0617b9c59739329d
Tim Janik <timj@gnu.org>
2015-05-28 23:47:51 +0200

    CYTHON: cimport std::shared_ptr

--- cython/Rapicorn.pyx
+++ cython/Rapicorn.pyx
@@ -3,3 +3,15 @@ from libcpp cimport *
 from cython.operator cimport dereference as deref
 ctypedef unsigned int uint
 
+# std::shared_ptr
+cdef extern from "memory" namespace "std":
+  cppclass shared_ptr[T]:
+    shared_ptr     ()
+    shared_ptr     (T)
+    shared_ptr     (shared_ptr[T]&)
+    void reset     ()
+    void swap      (shared_ptr&)
+    long use_count () const
+    bool unique    () const
+    T*   get       ()
+
