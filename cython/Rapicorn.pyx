5f6a8623753f7f3b274d0349545a5c1dccf9deec
Tim Janik <timj@gnu.org>
2015-05-28 23:47:24 +0200

    CYTHON: generate 'Rapicorn' python module

---
+++ cython/Rapicorn.pyx
@@ -0,0 +1,5 @@
+# This Source Code Form is licensed MPLv2: http://mozilla.org/MPL/2.0   -*-mode:python;-*-
+from libcpp cimport *
+from cython.operator cimport dereference as deref
+ctypedef unsigned int uint
+
