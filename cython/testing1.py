a35cbf12d299348639164fca56c1574ae06d2c6c
Tim Janik <timj@gnu.org>
2015-05-29 13:22:49 +0200

    CYTHON: add testing1.py for simple unit testing

---
+++ cython/testing1.py
@@ -0,0 +1,15 @@
+# Licensed CC0 Public Domain: http://creativecommons.org/publicdomain/zero/1.0
+import Rapicorn
+
+# test MainLoop and EventLoop object identities
+ml = Rapicorn.MainLoop()
+sl = ml.create_slave()
+sm = sl.main_loop()
+m3 = Rapicorn.MainLoop()
+assert ml == ml and sl == sl and sm == sm and m3 == m3
+assert ml != sl and sl != ml and sm != sl and sl != sm
+assert ml == sm and sm == ml and ml != m3 and m3 != ml
+assert ml.main_loop() == ml  and m3.main_loop() == m3
+
+# all done
+print '  %-6s' % 'CHECK', '%-67s' % __file__, 'OK'
