f58fcc684341008e27b88d654e41e9a4de5d27f0
Tim Janik <timj@gnu.org>
2015-06-01 01:51:21 +0200

    CYTHON: use xmanifold.py to generate PyxxCaller template variants

--- cython/pyxxutils.hh
+++ cython/pyxxutils.hh
@@ -1,26 +1,32 @@
 // This Source Code Form is licensed MPLv2: http://mozilla.org/MPL/2.0
 #include <rapicorn-core.hh>
 
-template<class R>
-class PyxxCaller0 {
-  typedef std::function<R ()> StdFunction;
-  typedef R (*Marshal) (PyObject*);
+/* PyObject caller template to use a pycallable as C++ std::function with
+ * varying numbers of arguments.
+ * Keep the 'ifdef XMANIFOLD_SECTION..endif XMANIFOLD_SECTION' markers in place,
+ * they are used by xmanifold.py to identify the template text.
+ */
+#ifdef  XMANIFOLD_SECTION
+template<class R, class A1, class A2>
+class PyxxCaller2 {
+  typedef std::function<R (A1, A2)> StdFunction;
+  typedef R (*Marshal) (PyObject*, A1, A2);
   Marshal   marshal_;
   PyObject *pyo_;
 public:
-  explicit PyxxCaller0 (PyObject *pyo, Marshal marshal) :
+  explicit PyxxCaller2 (PyObject *pyo, Marshal marshal) :
     marshal_ (marshal), pyo_ (pyo)
   {
     Py_INCREF (pyo_);
   }
-  /*copy*/ PyxxCaller0 (const PyxxCaller0 &other)
+  /*copy*/ PyxxCaller2 (const PyxxCaller2 &other)
   {
     marshal_ = other.marshal_;
     pyo_ = other.pyo_;
     Py_INCREF (pyo_);
   }
   void
-  operator= (const PyxxCaller0 &other)
+  operator= (const PyxxCaller2 &other)
   {
     marshal_ = other.marshal_;
     if (other.pyo_)
@@ -28,16 +34,19 @@ public:
     Py_XDECREF (pyo_);
     pyo_ = other.pyo_;
   }
-  ~PyxxCaller0()
+  ~PyxxCaller2()
   {
     Py_XDECREF (pyo_);
   }
   R
-  operator() ()
+  operator() (A1 a1, A2 a2)
   {
-    return marshal_ (pyo_);
+    return marshal_ (pyo_, a1, a2);
   }
 };
+#endif  // XMANIFOLD_SECTION
+
+#include "pyxxutils-xmany.hh" // generated from XMANIFOLD_SECTION
 
 /* PyxxCaller0<bool> is used for main loop handlers that will be disconnected if 'false'
  * is returned. When Python exceptions are thrown, the 'marshal_' default behaviour is
@@ -52,46 +61,9 @@ PyxxCaller0<bool>::operator() ()
   return result;
 }
 
-
-template<class R, class A1>
-class PyxxCaller1 {
-  typedef std::function<R (A1)> StdFunction;
-  typedef R (*Marshal) (PyObject*, A1);
-  Marshal   marshal_;
-  PyObject *pyo_;
-public:
-  explicit PyxxCaller1 (PyObject *pyo, Marshal marshal) :
-    marshal_ (marshal), pyo_ (pyo)
-  {
-    Py_INCREF (pyo_);
-  }
-  /*copy*/ PyxxCaller1 (const PyxxCaller1 &other)
-  {
-    marshal_ = other.marshal_;
-    pyo_ = other.pyo_;
-    Py_INCREF (pyo_);
-  }
-  void
-  operator= (const PyxxCaller1 &other)
-  {
-    marshal_ = other.marshal_;
-    if (other.pyo_)
-      Py_INCREF (other.pyo_);
-    Py_XDECREF (pyo_);
-    pyo_ = other.pyo_;
-  }
-  ~PyxxCaller1()
-  {
-    Py_XDECREF (pyo_);
-  }
-  R
-  operator() (A1 a1)
-  {
-    return marshal_ (pyo_, a1);
-  }
-};
-
-
+/* MainLoop watchdog source that quits the currently running loop upon a
+ * Python exception.
+ */
 static void
 pyxx_main_loop_add_watchdog (Rapicorn::MainLoop &main_loop)
 {
