f69efa62e1e080d7631614c59b75b89ad92382be
Tim Janik <timj@gnu.org>
2015-06-04 11:19:42 +0200

    CYTHON: provide Rapicorn.Application with main_loop, run, quit, execute

--- cython/Rapicorn.pyx
+++ cython/Rapicorn.pyx
@@ -16,9 +16,28 @@ include "idlapi.pyx"
 # Manual binding bits for rapicorn.hh
 cdef extern from "ui/clientapi.hh" namespace "Rapicorn":
   Rapicorn__Application     Rapicorn__init_app "Rapicorn::init_app" (const String &app_ident, int*, char**)
+  shared_ptr[Rapicorn__MainLoop] Rapicorn_ApplicationH_main_loop "Rapicorn::ApplicationH::main_loop" ()
 
-def init_app (app_ident):
+
+def init (app_name):
   import sys
+  def builtin_application_class():
+    global Application # this workaround is needed because cython confuses the existing
+    return Application # 'Application' class with the new overriding one we're introducing
+  class Application (builtin_application_class()):
+    def main_loop (self):
+      return Rapicorn__MainLoopP__wrap (Rapicorn_ApplicationH_main_loop())
+    def run (self):
+      """Execute the Application main event loop until it's quit, returns quit code."""
+      return self.main_loop().run()
+    def quit (self, quit_code = 0):
+      """Quit the currently running Application main event loop with quit_code."""
+      return self.main_loop().quit (quit_code)
+    def execute (self):
+      """Execute the Application and terminatie the program with sys.exit (Application.run())."""
+      sys.exit (self.run())
+  builtin_application_class().__aida_wrapper__ (Application)
   cdef int   argc = 1
   cdef char *argv = sys.argv[0]
-  return Rapicorn__Application__wrap (Rapicorn__init_app (app_ident, &argc, &argv))
+  # Application = RapicornApplication
+  return Rapicorn__Application__wrap (Rapicorn__init_app (app_name, &argc, &argv))
