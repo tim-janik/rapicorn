eaa6816c5f3b828fbf88978f562dbb6bd338b368
Tim Janik <timj@gnu.org>
2015-06-10 14:38:27 +0200

    UI: provide server side inserts for IDL structures

--- ui/serverinsert.hh
+++ ui/serverinsert.hh
@@ -4,7 +4,6 @@
 
 includes:
 #include <ui/utilities.hh>
-#include <ui/clientapi.hh>
 namespace Rapicorn {
 class WidgetImpl;
 class WindowImpl;
@@ -12,6 +11,34 @@ class WindowImpl;
 
 IGNORE:
 struct DUMMY { // dummy class for auto indentation
+
+class_scope:StringSeq:
+  explicit SrvT_StringSeq () {}
+  /*ctor*/ SrvT_StringSeq (const std::vector<std::string> &strv) : Sequence (strv) {}
+
+class_scope:Pixbuf:
+  /// Construct Pixbuf at given width and height.
+  explicit        SrvT_Pixbuf  (uint w, uint h) : row_length (0) { pixels.resize (w, h); }
+  /// Reset width and height and resize pixel sequence.
+  void            resize       (uint w, uint h) { row_length = w; pixels.resize (row_length * h); }
+  /// Access row as endian dependant ARGB integers.
+  uint32_t*       row          (uint y)         { return y < uint32_t (height()) ? (uint32_t*) &pixels[row_length * y] : NULL; }
+  /// Access row as endian dependant ARGB integers.
+  const uint32_t* row          (uint y) const   { return y < uint32_t (height()) ? (uint32_t*) &pixels[row_length * y] : NULL; }
+  /// Width of the Pixbuf.
+  int             width        () const         { return row_length; }
+  /// Height of the Pixbuf.
+  int             height       () const         { return row_length ? pixels.size() / row_length : 0; }
+
+class_scope:UpdateSpan:
+  explicit SrvT_UpdateSpan (int _start, int _length) : start (_start), length (_length) {}
+
+class_scope:UpdateRequest:
+  explicit SrvT_UpdateRequest (UpdateKind _kind, const SrvT_UpdateSpan &rs, const SrvT_UpdateSpan &cs = SrvT_UpdateSpan()) :
+    kind (_kind), rowspan (rs), colspan (cs) {}
+
+class_scope:Requisition:
+  inline SrvT_Requisition (double w, double h) : width (w), height (h) {}
 
 class_scope:Widget:
   WidgetImpl&       impl ();
@@ -25,5 +52,4 @@ IGNORE: // close last _scope
 }; // close dummy class scope
 
 global_scope:
-// #define RAPICORN_PIXBUF_TYPE    Rapicorn::Pixbuf
 #include <ui/pixmap.hh>
