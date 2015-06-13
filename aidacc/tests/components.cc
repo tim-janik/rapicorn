f19dc84e293937ac83bf417353ae39291d60699f
Tim Janik <timj@gnu.org>
2015-06-13 13:24:45 +0200

    AIDACC: tests: test compilation of various argument and return types

--- aidacc/tests/components.cc
+++ aidacc/tests/components.cc
@@ -1,13 +1,16 @@
 // This Source Code Form is licensed MPLv2: http://mozilla.org/MPL/2.0
 #include <stdio.h>
-#include "components-a1-client.hh"
 #include "components-a1-server.hh"
+#include "components-a1-client.hh"
+
+using namespace Rapicorn;
 
 int
 main (int   argc,
       char *argv[])
 {
-  printf ("  %-8s %-60s  %s\n", "RUN", argv[0], "OK");
+  printout ("  %-8s %-60s  %s\n", "TEST", argv[0], "OK");
+
   return 0;
 }
 
