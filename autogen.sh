422bb2d8b528f06ab76758aa27a6b24b831160d7
Tim Janik <timj@gnu.org>
2015-10-09 11:22:45 +0200

    BUILD: autogen.sh: simplify to the bare minimum required
    Signed-off-by: Tim Janik <timj@gnu.org>

--- autogen.sh
+++ autogen.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 
-echo "$0: Cleaning configure caches..."
-rm -rf autom4te.cache/
-rm -f  config.cache
+set -xe # be verbose and abort on errors
 
-echo "$0: autoreconf -vfsi -Wno-portability"
-autoreconf -vfsi -Wno-portability || exit $?
+rm -rf autom4te.cache/ config.cache
 
-# intltool-extract.in intltool-merge.in intltool-update.in po/Makefile.in.in
-echo "$0: intltoolize --force --automake"
-intltoolize --force --automake
+autoreconf -vfsi
 
-echo "$0: Override po/Makefile.in.in with po/Makefile.intltool"
-rm -f po/Makefile.in.in && cp -v po/Makefile.intltool po/Makefile.in.in || exit $?
+intltoolize --force --automake # intltool-extract.in intltool-merge.in intltool-update.in po/Makefile.in.in
+rm -f po/Makefile.in.in && cp -v po/Makefile.intltool po/Makefile.in.in # fix po/Makefile...
 
-echo "$0: ./configure $*"
-./configure "$@" || exit $?
+./configure "$@"
