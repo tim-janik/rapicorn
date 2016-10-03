ece961a03c7589011d680c45afce0b8d24e919aa
Tim Janik <timj@gnu.org>
2016-10-03 23:53:31 +0200

    BUILD: autogen.sh: spell out the autoreconf arguments for clarity
    Signed-off-by: Tim Janik <timj@gnu.org>

--- autogen.sh
+++ autogen.sh
@@ -4,7 +4,7 @@ set -xe # be verbose and abort on errors
 
 rm -rf autom4te.cache/ config.cache
 
-autoreconf -vfsi
+autoreconf --verbose --force --symlink --install
 
 intltoolize --force --automake # intltool-extract.in intltool-merge.in intltool-update.in po/Makefile.in.in
 rm -f po/Makefile.in.in && cp -v po/Makefile.intltool po/Makefile.in.in # fix po/Makefile...
