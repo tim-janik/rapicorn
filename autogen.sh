042f73096395f9b5a98e9977b3d0bb7e58329ed8
Tim Janik <timj@gnu.org>
2016-10-06 17:26:46 +0200

    BUILD: remove 'devel-mode' setting, infer development aids from ~wip versions
    Signed-off-by: Tim Janik <timj@gnu.org>

--- autogen.sh
+++ autogen.sh
@@ -9,4 +9,4 @@ autoreconf --verbose --force --symlink --install
 intltoolize --force --automake # intltool-extract.in intltool-merge.in intltool-update.in po/Makefile.in.in
 rm -f po/Makefile.in.in && cp -v po/Makefile.intltool po/Makefile.in.in # fix po/Makefile...
 
-./configure --enable-devel-mode=yes "$@"
+./configure "$@"
